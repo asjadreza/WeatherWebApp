d3c2930f92610e4469844a710750ec3acc0394c0
Md Asjad Reza <asjadreza64@gmail.com>
2023-07-23 23:08:41 +0530

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,24 @@ const url = require('url');
 const API_URL = process.env.API_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY = process.env.API_KEY;
+// const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 router.get('/', async (req, res) => {
     console.log(url.parse(req.url, true).query)
+
+    const unit_name = "&units=";
+    const unit_value = "metric";
+    const unit = unit_name + unit_value;
 
     try {
         const params = new URLSearchParams({
             [API_KEY_NAME]: API_KEY,
             ...url.parse(req.url, true).query,
         })
-        console.log(params);
-        const apiResponse = await needle('get', `${API_URL}?${params}`)
+        const apiResponse = await needle('get', `${API_URL}?${params}` + unit)
+        console.log(apiResponse);
         const data = apiResponse.body;
+        console.log(data);
         if(process.env.NODE_ENV !== 'production'){
             console.log(`REQUEST: ${API_URL}?${params}`)
         }
